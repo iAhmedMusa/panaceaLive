2835a0d36ecfddcea9bff990450e5fe1dce34c95
Ahmed Musa <ahmusa@ymail.com>
2020-03-28 18:34:02 +0600

    updated seeder file

--- database/seeds/ProductsCategorySeeder.php
+++ database/seeds/ProductsCategorySeeder.php
@@ -13,10 +13,10 @@ class ProductsCategorySeeder extends Seeder
     {
         DB::table('products_category')->delete();
         $values = array(
-            [ 'cat_id' => 1, 'cat_name' => 'Bike', 'warranty_length' => '02-02-2023',  'created_at' => now(),'updated_at' => now() ],
-            [ 'cat_id' => 2, 'cat_name' => 'Cosmetic', 'warranty_length' => '07-02-2024',  'created_at' => now(),'updated_at' => now() ],
-            [ 'cat_id' => 3, 'cat_name' => 'Medicine', 'warranty_length' => '05-02-2025',  'created_at' => now(),'updated_at' => now() ],
-            [ 'cat_id' => 4, 'cat_name' => 'Home Appliance', 'warranty_length' => '03-02-2026',  'created_at' => now(),'updated_at' => now() ]
+            [ 'cat_id' => 1, 'cat_name' => 'Bike', 'warranty_length' => '36',  'created_at' => now(),'updated_at' => now() ],
+            [ 'cat_id' => 2, 'cat_name' => 'Cosmetic', 'warranty_length' => '1.5',  'created_at' => now(),'updated_at' => now() ],
+            [ 'cat_id' => 3, 'cat_name' => 'Medicine', 'warranty_length' => '24',  'created_at' => now(),'updated_at' => now() ],
+            [ 'cat_id' => 4, 'cat_name' => 'Home Appliance', 'warranty_length' => '12',  'created_at' => now(),'updated_at' => now() ]
         
         );
         DB::table('products_category')->insert($values);
